3d4b7c9dc918a687903d1ddb4b9e0540dc14081b
jai balaji <jai.balaji@cerner.com>
2020-08-06 18:40:22 +0530

    getby where condition

--- src/api/person/personController.js
+++ src/api/person/personController.js
@@ -16,7 +16,18 @@ const addPerson = (req, res, next)=>{
 };
 
 const getPerson = (req, res, next)=>{
-
+  Person.findOne({name: req.params.value})
+      .then((post)=>{
+        if(!post) {
+          const err = new Error('post not found');
+          err.statusCode  = 404;
+          throw err;
+        }
+        res.status(200).send({message: 'success', post: post});
+      })
+      .catch((err)=>{
+        console.log('error while parsing the findOne method', err);
+      });
 };
 
 export {getPerson, addPerson};
--- src/api/person/personRouter.js
+++ src/api/person/personRouter.js
@@ -4,8 +4,8 @@ import * as personController from './personController';
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// GET : /person/:id
-router.get('/:id', personController.getPerson);
+// GET : /person/type/value ex: /person/name/jaibalaji
+router.get('/:type/:value', personController.getPerson);
 
 // // POST : /person/add-person
 router.post('/add-person', createPersonValidator, personController.addPerson);
